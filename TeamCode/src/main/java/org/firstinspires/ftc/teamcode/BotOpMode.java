7fbe418a4a05830ca74b3cfa8a80fc2657a77403
anshpatel20202009-creator <anshpatel20202009@gmail.com>
2025-10-07 16:47:09 -0700

    BotOpMode
    Hello

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotOpMode.java
@@ -1,47 +1,36 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.Commands.IntakeMotorCommand;
+import org.firstinspires.ftc.teamcode.Commands.IntakeServoCommand;
+import org.firstinspires.ftc.teamcode.Commands.IntakeStopServoCommand;
+import org.firstinspires.ftc.teamcode.Subsystems.IntakeMotorSubsystem;
+import org.firstinspires.ftc.teamcode.Subsystems.IntakeServoSubsystem;
+@TeleOp
 
-@TeleOp(name = "Shooter control code", group = "prototype")
-public class ProtypeShooterControl extends LinearOpMode {
 
+public class BotOpMode extends LinearOpMode {
     public DcMotor shooterMotor = null;
     private double shooterPower;
     public Servo shooterAngle = null;
+
+    private IntakeMotorSubsystem motorSubsystem;
+    private IntakeServoSubsystem servoSubsystem;
+    private IntakeMotorCommand motorCommand;
+    private IntakeServoCommand servoCommand;
+    private IntakeStopServoCommand stopServoCommand;
+
+
 
     @Override
     public void runOpMode() {
@@ -57,6 +46,25 @@ public class ProtypeShooterControl extends LinearOpMode {
         shooterMotor.setDirection(DcMotor.Direction.FORWARD);
 
         shooterAngle = hardwareMap.get(Servo.class, "shooterAngle");
+        Motor frontLeft = new Motor(hardwareMap,"frontLeft");
+        Motor frontRight = new Motor(hardwareMap,"frontRight");
+        Motor backLeft = new Motor(hardwareMap, "backLeft");
+        Motor backRight = new Motor(hardwareMap, "backRight");
+        motorSubsystem = new IntakeMotorSubsystem(frontLeft,frontRight,backLeft,backRight);
+
+        servoSubsystem = new IntakeServoSubsystem(hardwareMap.get(CRServo.class, "servo"), hardwareMap.get(CRServo.class, "servo2"));
+
+        motorCommand = new IntakeMotorCommand(
+                motorSubsystem,
+                () -> gamepad1.left_stick_x,
+                () -> gamepad1.left_stick_y,
+                () -> gamepad1.right_stick_x
+        );
+
+        servoCommand = new IntakeServoCommand(servoSubsystem);
+        stopServoCommand = new IntakeStopServoCommand(servoSubsystem);
+
+        CommandScheduler.getInstance().schedule(motorCommand);
 
         waitForStart();
 
@@ -66,6 +74,10 @@ public class ProtypeShooterControl extends LinearOpMode {
 
 
         while (opModeIsActive()) {
+
+            CommandScheduler.getInstance().run();
+            
+
             newShooterPower = shooterPower;
 
             if (gamepad1.dpadUpWasPressed()) {
@@ -85,7 +97,7 @@ public class ProtypeShooterControl extends LinearOpMode {
                 newServoAngle -= servoAngleChange;
             }
 
-            if(gamepad1.a) {
+            if (gamepad1.a) {
                 shooterPower += .05;
                 shooterMotor.setPower(shooterPower);
             }
@@ -99,7 +111,6 @@ public class ProtypeShooterControl extends LinearOpMode {
 //            }
 
 
-
             if ((newServoAngle > 0.0) && (newServoAngle < 1.0)) {
                 launchServoAngle = newServoAngle;
                 shooterAngle.setPosition(launchServoAngle);
@@ -110,6 +121,7 @@ public class ProtypeShooterControl extends LinearOpMode {
             telemetry.addData("shooterLaunchPosition", "%.3f", launchServoAngle);
             telemetry.update();
         }
-
     }
 }
+
+
