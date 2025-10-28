1baa75cd981f46831cb1bc076f78cb46d26bf583
peytonh60 <145856203+peytonh60@users.noreply.github.com>
2025-10-28 13:43:01 -0400

    op
    updates opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/IntakeMotorCommand.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.Commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+
+import org.firstinspires.ftc.teamcode.Subsystems.IntakeMotorSubsystem;
+
+import java.util.function.DoubleSupplier;
+
+public class IntakeMotorCommand extends CommandBase {
+    private IntakeMotorSubsystem driveSubsystem;
+    private DoubleSupplier strafe, forward, turn, heading;
+    private MecanumDrive drive;
+    private boolean fieldCentric;
+
+    public IntakeMotorCommand(IntakeMotorSubsystem driveSubsystem, DoubleSupplier strafe, DoubleSupplier forward, DoubleSupplier turn){
+        this.driveSubsystem = driveSubsystem;
+        this.strafe = strafe;
+        this.forward = forward;
+        this.turn = turn;
+
+        addRequirements(driveSubsystem);
+
+    }
+    @Override
+    public void execute(){
+        driveSubsystem.drive(strafe.getAsDouble(),forward.getAsDouble(),turn.getAsDouble())  ;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/IntakeServoCommand.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.Commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.Subsystems.IntakeServoSubsystem;
+
+public class IntakeServoCommand extends CommandBase {
+    private IntakeServoSubsystem subsystem;
+    public IntakeServoCommand(IntakeServoSubsystem subsystem ){
+        this.subsystem = subsystem;
+addRequirements(subsystem);
+    }
+    @Override
+    public void execute() {
+        subsystem.runServo();
+    }
+}
