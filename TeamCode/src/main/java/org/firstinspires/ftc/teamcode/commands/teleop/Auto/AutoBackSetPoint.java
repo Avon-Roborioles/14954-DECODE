8cfc86a7523fbd0fd786ef9feb759bbd8dd8350d
TornadoBoah <nboekekitt@gmail.com>
2025-12-03 23:16:48 -0500

    Added SetBackPoint for auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/teleop/Auto/AutoBackSetPoint.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.commands.teleop.Auto;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.util.Timing;
+
+import org.firstinspires.ftc.teamcode.Subsystems.LaunchSubsystem;
+
+import java.util.concurrent.TimeUnit;
+
+public class AutoBackSetPoint extends CommandBase {
+    private LaunchSubsystem launchSubsystem;
+    private Timing.Timer timer;
+
+    public AutoBackSetPoint(LaunchSubsystem launchSubsystem){
+        this.launchSubsystem = launchSubsystem;
+        addRequirements(launchSubsystem);
+    }
+
+    public void initialize(){
+        timer = new Timing.Timer(2000, TimeUnit.MILLISECONDS);
+        timer.start();
+        launchSubsystem.backSetPoint();
+    }
+    public boolean isFinished(){
+        return timer.done();
+    }
+}
