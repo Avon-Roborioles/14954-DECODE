feb9476dd4ec32cf17d3d30f1c10c295f735e4c4
TornadoBoah <nboekekitt@gmail.com>
2026-02-16 15:08:35 -0500

    inproved teleop system

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/newTeleOpBlue.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class newTeleOpBlue extends TeleOpBase{
+    @Override
+    public Boolean redAlliance() {
+        return false;
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/newTeleOpRed.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class newTeleOpRed extends TeleOpBase{
+    @Override
+    public Boolean redAlliance() {
+        return true;
+    }
+}
